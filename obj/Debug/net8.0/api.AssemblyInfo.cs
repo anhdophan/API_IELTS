49162422cfe5af368fdf31fd9ff783c9ff2e9cb4
DoPhan <07phananhdo@gmail.com>
2025-07-05 09:57:08 +0700

    fix Questions FE bug

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92925562ff6d85c2d25996805670aba30ca7647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f41d34f2315f973d1b790451e090834f5579eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
