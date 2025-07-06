0c12718e0cc1f55edccb0fed1225de634db82bba
DoPhan <07phananhdo@gmail.com>
2025-07-06 19:37:10 +0700

    Update Exam model

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1d02fe599b5f390ccc72721b50552447b4a805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc87d7647f7c03af734c4065004e63ba0ff0ea1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
